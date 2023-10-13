7762868640a43bc43f76a5ca22a6eeade4a85a93
aldekal <ahmed@aldekal.de>
2023-10-13 18:02:51 +0200

    pub repo

--- clone-data-repo.sh
+++ clone-data-repo.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-echo "checking if ssh key for the atlas content repo is present"
-FILE=/run/secrets/ssh_secret
-# if ssh key is not empty, init the db with data:
-if test -f "$FILE"; then
-    echo "ssh key present"
-# add ssh key and add github as known host
-    mkdir /root/.ssh/ && cp /run/secrets/ssh_secret /root/.ssh/id_rsa
-    chmod 400 /root/.ssh/id_rsa && ssh-keyscan github.com >> /root/.ssh/known_hosts
+echo "try to clone the content repository from ${QC_ATLAS_CONTENT_REPOSITORY_URL} to ${QC_ATLAS_CONTENT_REPOSITORY_PATH}"
+
 # clone repo and copy the file with the sql statements to the folder that is executed on startup of the postgres db
     git clone ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
 	if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}" ]; then
@@ -14,6 +8,3 @@ if test -f "$FILE"; then
     else
 	    echo "unable to find specified directory with example data in the repository"
 	fi
-else
-    echo "ssh key not present, proceeding with empty db"
-fi
